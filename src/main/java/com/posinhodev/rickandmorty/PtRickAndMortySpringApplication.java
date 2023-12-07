818a4d0f873bbfa1190642fd86ec8eabc5fbf630
posinhodev <jose.possop12@gmail.com>
2023-12-06 19:02:41 -0500

    First commit

---
+++ src/main/java/com/posinhodev/rickandmorty/PtRickAndMortySpringApplication.java
@@ -0,0 +1,13 @@
+package com.posinhodev.rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtRickAndMortySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtRickAndMortySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posinhodev/rickandmorty/PtRickAndMortySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posinhodev.rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtRickAndMortySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
